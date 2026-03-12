36c19e5ee2153b75398858866c98de092343024c
Delfi-CH <delfi@delfi.dev>
2026-03-12 18:21:25 +0100

    feat: add style to system without download

--- src/lib/components/OsViewer.svelte
+++ src/lib/components/OsViewer.svelte
@@ -9,7 +9,7 @@
 <main>
     <h1>{os.name}</h1>
     <ul>
-        <li>Latest Version: {os.version}</li>
+        <li>Version: {os.version}</li>
         <li>Family: {os.family}</li>
         <li>Webpage: <a href={os.homepage} target="_blank">{os.homepage}</a></li>
         <li>Supported: {os.isSupported ? "Yes" : "No"}</li>
@@ -17,10 +17,20 @@
     </ul>
     {#if os.imageDownloadURL} 
         <Downloader os={os}></Downloader>
+    {:else}
+        <p class="noDownload">No download available</p>
     {/if}
     <p>{os.description}</p>
 </main>
 
 <style>
-
+    .noDownload {
+        background-color: #0042FF;
+        padding: 0.4rem;
+        color: white;
+        width: fit-content;
+        border: 5px solid blue;
+        border-radius: 2rem;
+        cursor: not-allowed;
+    }
 </style>
