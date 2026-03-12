96fb71c85901654c03424583e56b18826790fb72
Delfi-CH <laspitzer@gmx.ch>
2026-03-12 11:22:16 +0100

    refactor: remove greet from main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,9 @@
 <script>
 // @ts-nocheck
 
-  import { invoke } from "@tauri-apps/api/core";
   import {getAllOs } from "$lib/fetch.js"
   import { onMount } from 'svelte';
   import OsViewer from "$lib/components/OsViewer.svelte";
-
-  let name = $state("");
-  let greetMsg = $state("");
-
-  async function greet(event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
 
   let data = $state([{}])
 
@@ -26,12 +16,6 @@
 
 <main class="container">
   <h1>sysfind</h1>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
 
   {#each data as os (os.id)}
     <OsViewer os={os}></OsViewer>
